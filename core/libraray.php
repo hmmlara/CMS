9e025f89e10af02e6d1f5c237db7d4f8bcfbc5b6
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-01 18:40:20 +0630

    add search data function

--- core/libraray.php
+++ core/libraray.php
@@ -3,3 +3,23 @@
 function public_path(){
     return dirname(__DIR__);
 }
+
+function search_data($array, $search_val){
+
+    $body = [];
+    $keys = [];
+    // get key from array
+    foreach($array[0] as $key => $value){
+        array_push($keys,$key);
+    }
+
+    foreach($array as $key => $value){
+        // get array when data is search
+        foreach($keys as $key){
+            if($value[$key] == $search_val){
+                array_push($body,$value);
+            }
+        }
+    }
+    return $body;
+}
