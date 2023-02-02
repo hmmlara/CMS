ba9c9d4c796dd3cfe442b3cd1e5348d9dbd8920e
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-02 22:04:37 +0630

    remove condition for str contain

--- core/libraray.php
+++ core/libraray.php
@@ -1,23 +1,25 @@
-<?php 
+<?php
 
-function public_path(){
+function public_path()
+{
     return dirname(__DIR__);
 }
 
-function search_data($array, $search_val){
+function search_data($array, $search_val)
+{
 
     $body = [];
     $keys = [];
     // get key from array
-    foreach($array[0] as $key => $value){
-        array_push($keys,$key);
+    foreach ($array[0] as $key => $value) {
+        array_push($keys, $key);
     }
 
-    foreach($array as $key => $value){
+    foreach ($array as $key => $value) {
         // get array when data is search
-        foreach($keys as $key){
-            if(strcasecmp($value[$key],$search_val) == 0 || strpos($value[$key],$search_val) !== false){
-                array_push($body,$value);
+        foreach ($keys as $key) {
+            if (strcasecmp($value[$key],$search_val) == 0) {
+                array_push($body, $value);
             }
         }
     }
