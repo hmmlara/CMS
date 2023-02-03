8b3e4ac1918ca4d34aa532f58278f43277741fb9
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-03 21:12:27 +0630

    change directory of denpenency file

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,6 +1,6 @@
 <script src="js/mdb.min.js"></script>
 <script src="vendor/jquery/jquery.min.js"></script>
-<script src='js/dist/index.global.js'></script>
+<script src='js/index.global.js'></script>
 <script src="js/main.js"></script>
 <script src="js/delete.js"></script>
 <script src="js/schedule.js"></script>
