64632e7d313293417d152d1b2a418dc7199ff7d1
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-03 21:08:27 +0630

    change align of code

--- layouts/header.php
+++ layouts/header.php
@@ -7,19 +7,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
 
+    <link rel="stylesheet" href="css/mdb.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
         integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/mdb.min.css">
-    <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
 
-  <!-- navbar start -->
+    <!-- navbar start -->
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-        <a class="navbar-brand text-dark" href="index.php">CMS</a>
+            <a class="navbar-brand text-dark" href="index.php">CMS</a>
             <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fa fa-list text-white"></i>
@@ -34,10 +35,10 @@
                         <a class="nav-link mx-2" aria-current="page" href="index">Dashboard</a>
                     </li>
 
-                        <li class="nav-item <?php echo (strpos($page,'patient') !== false )? 'active' : ''; ?>">
+                    <li class="nav-item <?php echo (strpos($page,'patient') !== false )? 'active' : ''; ?>">
                         <a class="nav-link mx-2" href="all_patients">Patients</a>
                     </li>
-                    
+
                     <li class="nav-item <?php echo (strpos($page,'doctor') !== false )? 'active' : '';?>">
                         <a class="nav-link mx-2" href="all_doctors">Doctors</a>
                     </li>
