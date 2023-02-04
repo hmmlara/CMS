2d21d03897579e59e0e080277a05bf1ca512c976
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-04 19:48:16 +0630

    change positon of parent script tag

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,6 +1,3 @@
-<script src="js/mdb.min.js"></script>
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src='js/index.global.js'></script>
 <script src="js/main.js"></script>
 <script src="js/delete.js"></script>
 <script src="js/schedule.js"></script>
