bc5d3e5cc5af4883320ab08bbf0df130ac08ade0
chotay13599 <chotay13599@gmail.com>
2023-02-09 12:06:15 +0630

    add specility

--- doctor_info.php
+++ doctor_info.php
@@ -9,9 +9,11 @@ $doctorController=new DoctorController();
 $doctors=$doctorController->getDoctors();
 
 $gender=["","Female","male"];
+$specialities=["","General Medicine","Nephrologists","Paediatrician","Physical Medicine","Mental Health","Obsteric & Gynae"];
 
 if(isset($_GET["id"])){
     $doctorInfos =$doctorController->getDetail($_GET["id"]);
+    // var_dump($doctorInfos);
 
     if(!$doctorInfos){
         echo "<script>
@@ -68,7 +70,11 @@ if(isset($_GET["id"])){
                 </li>
                 <li class="nav-item" role="presentation">
                     <a class="nav-link fw-bold" id="ex2-tab-2" data-mdb-toggle="tab" href="#ex2-tabs-2" role="tab"
-                        aria-controls="ex2-tabs-2" aria-selected="false">Biography</a>
+                        aria-controls="ex2-tabs-2" aria-selected="false">Information</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link fw-bold" id="ex2-tab-2" data-mdb-toggle="tab" href="#ex2-tabs-2" role="tab"
+                        aria-controls="ex2-tabs-2" aria-selected="false">Treatment</a>
                 </li>
             </ul>
             <!-- Tabs navs -->
@@ -76,7 +82,7 @@ if(isset($_GET["id"])){
             <!-- Tabs content -->
             <div class="tab-content" id="ex2-content">
                 <div class="tab-pane fade show active" id="ex2-tabs-1" role="tabpanel" aria-labelledby="ex2-tab-1">
-                    <table class="table  table-active table-striped mt-5 text-center text-dark">
+                    <!-- <table class="table  table-active table-striped mt-5 text-center text-dark">
                         <thead class="bg-gradient-dark   ">
                             <th>Id</th>
                             <th>Patient Name</th>
@@ -91,7 +97,7 @@ if(isset($_GET["id"])){
                                 <td>-</td>
                             </tr>
                         </tbody>
-                    </table>
+                    </table> -->
                 </div>
                 <div class="tab-pane fade overflow-auto" id="ex2-tabs-2" role="tabpanel" aria-labelledby="ex2-tab-2"
                     style="min-height: 300px;">
@@ -102,7 +108,7 @@ if(isset($_GET["id"])){
                                     <div class="col-md-6">
                                         <h5 class='mb-4'>Doctor
                                             Code:&nbsp;&nbsp;<span><?php echo $doctorInfos['user_code'] ?></span></h5>
-                                        <h5 class='mb-4'>Age:&nbsp;&nbsp;<span><?php echo $doctorInfos['age'] ?></span>
+                                        <h5 class='mb-4'>Date of Birth:&nbsp;&nbsp;<span><?php echo $doctorInfos['age'] ?></span>
                                         </h5>
                                         <h5 class='mb-4'>
                                             Education:&nbsp;&nbsp;<span><?php echo $doctorInfos['education'] ?></span>
@@ -112,14 +118,27 @@ if(isset($_GET["id"])){
                                         </h5>
                                     </div>
                                     <div class="col-md-6">
+
                                         <h5 class='mb-4'>
                                             martial_status:&nbsp;&nbsp;<span><?php echo $doctorInfos['martial_status'] ?></span>
                                         </h5>
-                                        <h5 class='mb-4'>Nrc:&nbsp;&nbsp;<span><?php echo $doctorInfos['nrc'] ?></span>
-                                        </h5>
+
                                         <h5 class='mb-4'>
-                                            created_at:&nbsp;&nbsp;<span><?php echo $doctorInfos['created_at'] ?></span>
+                                            Nrc:&nbsp;&nbsp;<span><?php echo $doctorInfos['nrc'] ?></span>
                                         </h5>
+
+                                        <!-- <h5 class='mb-4'>
+                                            created_at:&nbsp;&nbsp;<span><?php echo $doctorInfos['created_at'] ?></span>
+                                        </h5> -->
+
+                                        <h5 class='mb-4'>
+                                            Phone:&nbsp;&nbsp;<span><?php echo $doctorInfos['phone'] ?></span>
+                                        </h5>
+
+                                        <h5 class='mb-4'>
+                                            Speciality:&nbsp;&nbsp;<span><?php echo $specialities[$doctorInfos['specialities']] ?></span>
+                                        </h5>
+
                                         <h5 class='mb-4'>
                                             Updated_at:&nbsp;&nbsp;<span><?php echo $doctorInfos['updated_at'] ?></span>
                                         </h5>
