5bf42bbafa926139f849b663ce4030f965887e99
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-15 15:04:47 +0630

    update code

--- models/Appointment.php
+++ models/Appointment.php
@@ -6,28 +6,56 @@ class Appointment{
 
     private $pdo;
 
-    //add appointment
-    // protected function addData($data){
-    //     $this->pdo=Database::connect();
+    //get Appointments
+    protected function getAppoints($date){
+        $this->pdo = Database::connect();
+        
+        $query = 'select appointments.*,patients.pr_code as pr_name,user_infos.name as dr_name
+                from appointments join patients on patients.id = appointments.pr_id
+                join user_infos on user_infos.user_id = appointments.user_id
+                where appointments.appointment_date = :date and status = 0';
+        
+        $statement = $this->pdo->prepare($query);
 
-    //     $sql="";
-    // }
+        $statement->bindParam(':date',$date);
 
-    //get AppointmentTimes
-    protected function getTimes(){
-        $this->pdo=Database::connect();
-
-        $sql="SELECT schedules.shift_day,schedules.shift_start,schedules.shift_end,user_infos.name
-        from schedules join user_infos
-        where schedules.user_id = user_infos.user_id";
-
-        $statement=$this->pdo->prepare($sql);
         $statement->execute();
 
-        $result=$statement->fetchAll(PDO::FETCH_ASSOC);
-        return $result;
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
 
+    //add appointment
+    protected function addData($data){
 
+        $data['status'] = 0;
+        $data['created_at'] = date('Y-m-d');
+        $data['updated_at'] = date('Y-m-d');
+
+        $this->pdo=Database::connect();
+
+        $query = "INSERT INTO `appointments`(`pr_id`, `user_id`, `appointment_date`, `appointment_time`, `status`, `created_at`, `updated_at`) 
+                VALUES (:pr_id, :user_id, :appointment_date, :appointment_time, :status, :created_at, :updated_at)";
+
+        $statement = $this->pdo->prepare($query);
+
+        foreach($data as $key => $value){
+            $statement->bindParam(":$key", $data[$key]);
+        }
+
+        return $statement->execute();
+    }
+
+    // delete appointment
+    protected function delAppoint($id){
+        $this->pdo = Database::connect();
+
+        $query = "delete from appointments where id = :id";
+
+        $statement = $this->pdo->prepare($query);
+
+        $statement->bindParam(":id",$id);
+
+        return $statement->execute();
     }
 
 }
