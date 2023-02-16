cd7f512963ca226c6d4dfa84f4025e42f4529f6f
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-16 14:42:20 +0630

    new added file

---
+++ appointment_prescription.php
@@ -0,0 +1,12 @@
+<?php 
+    include_once './layouts/header.php';
+?>
+
+<div class="container m-3">
+    
+    <h3 class="">Prescription</h3>
+</div>
+
+<?php 
+    include_once './layouts/footer.php';
+?>
