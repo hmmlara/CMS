70af7c0b93e46438fd9e93a6a9237bb67a652880
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-16 14:41:21 +0630

    change some code

--- controllers/AppointmentController.php
+++ controllers/AppointmentController.php
@@ -1,22 +1,30 @@
 <?php
-    include_once __DIR__."/../models/Appointment.php";
+include_once __DIR__ . "/../models/Appointment.php";
 
+class AppointmentController extends Appointment
+{
 
-    class AppointmentController extends Appointment{
-
-        //add appointment
-        public function add($data){
-            return $this->addData($data);
-        }
-            
-        //get appoint_times
-        public function getAppointments($date){
-            return $this->getAppoints($date);
-        }
-
-        // delete appoint
-        public function delete($id){
-            return $this->delAppoint($id);
-        }
+    //add appointment
+    public function add($data)
+    {
+        return $this->addData($data);
     }
-?>
+
+    //get appoint_times
+    public function getAppointments($date)
+    {
+        return $this->getAppoints($date);
+    }
+
+    // update status
+    public function update($id, $status_code)
+    {
+        return $this->updateStatus($id, $status_code);
+    }
+
+    // delete appoint
+    public function delete($id)
+    {
+        return $this->delAppoint($id);
+    }
+}
