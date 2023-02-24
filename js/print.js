f2c2a91efd49ff022cdb76af560a7d0fa10e6ab9
Pyae Phyo Zan <pyae418283@gmail.com>
2023-02-24 13:09:29 +0630

    update code

--- js/print.js
+++ js/print.js
@@ -1,23 +1,40 @@
 $('#check').click(function () {
 
+    // alert($('#service_id').text());
+    let data = {
+        'treatment_id': $('#treatment_id').text(),
+        'amount': $('#total').text(),
+        'invoice_code': $('#invoice_code').text(),
+        'service_id' : $('#service_id').text()
+    };
     $.ajax({
         type: "POST",
         url: "create_invoice",
         data: {
-            data: {
-                'treatment_id': $('#treatment_id').text(),
-                'amount': $('#total').text()
-            }
+            data: data
         },
         success: function (response) {
+            // console.log(JSON.parse(response));
             if(response == 'success'){
-                window.print();
-
-                window.location.href = `add_appointment`
+                onPrint();
             }
             else{
+                console.log(response);
                 alert('There is an erro!');
             }
         }
     });
 });
+
+function onPrint(){
+    window.onafterprint = function(e){
+        $(window).off('mousemove', window.onafterprint);
+        window.location.href = 'invoices';
+    };
+
+    window.print();
+
+    setTimeout(function(){
+        $(window).one('mousemove', window.onafterprint);
+    }, 1);
+}
