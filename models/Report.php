da6d78e50f98ccd159f9dedbf79cf113bb2f7237
chotay13599 <chotay13599@gmail.com>
2023-03-03 14:04:06 +0630

    new added

--- models/Report.php
+++ models/Report.php
@@ -25,4 +25,23 @@ class Report
 
         return $statment->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    protected function getDailyQty(){
+        $this->pdo=Database::connect();
+
+        $query="SELECT MONTHNAME(MAX(treatment_date)) AS month,YEAR(treatment_date) as year,COUNT(treatments.treatment_date) as treatments,treatments.treatment_date,
+        SUM(payments.amount) as income
+        FROM treatments
+        JOIN payments ON payments.treatment_id = treatments.id
+    
+        GROUP BY treatments.treatment_date";
+
+        $statment=$this->pdo->prepare($query);
+
+        // $statment->bindParam(':month',$month);
+
+        $statment->execute();
+
+        return $statment->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
