7e2c0e780c5091dfc401faa6d4e27bfb38e4151d
FeryDee <eimonkyaw5521@gmail.com>
2023-03-09 12:55:13 +0630

    something change

--- medi_stock_his.php
+++ medi_stock_his.php
@@ -21,42 +21,42 @@ for ($i = 1; $i <= count($mediStocks); $i++) {
 ?>
 
 <div class="container mt-5">
-<div clss="row">
-<div class="row">
-    <div class="col-8">
-        <h5 class="mb-4">Stock Medicine History</h5>
-    </div>
-    <div class="col-4 mb-3">
-    </div>
+    <div clss="row">
+        <div class="row">
+            <div class="col-8">
+                <h5 class="mb-4">Stock Medicine History</h5>
+            </div>
+            <div class="col-4 mb-3">
+            </div>
 
-    <div class="row">
-        <div class="col-11 d-flex justify-content-between  mb-3">
+            <div class="row">
+                <div class="col-11 d-flex justify-content-between  mb-3">
+                </div>
+                <div class="col-1 me">
+                    <a href="stock_medicine.php" class="text-dark text-decoration-underline">Back</a>
+                </div>
+            </div>
+
+            <hr class="hr-blurry">
         </div>
-        <div class="col-1 me">
-           <a href="stock_medicine.php" class="text-dark text-decoration-underline">Back</a>
-        </div>
-    </div>
 
-    <hr class="hr-blurry">
-</div>  
+        <div class="col-12 d-flex">
 
-<div class="col-12 d-flex">
-
-    <table class="table text-center">
-        <thead class="table-dark">
-            <tr>
-                <th>Id</th>
-                <th>Medicine_Name</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Man_Date</th>
-                <th>Exp Date</th>
-                <th>Enter Date</th>
-                <th>Functions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
+            <table class="table text-center">
+                <thead class="table-dark">
+                    <tr>
+                        <th>Id</th>
+                        <th>Medicine_Name</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Man_Date</th>
+                        <th>Exp Date</th>
+                        <th>Enter Date</th>
+                        <th>Functions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
                     foreach($mediStocks as $stockMedicine)
                     {
                         echo "<tr>";
@@ -68,18 +68,18 @@ for ($i = 1; $i <= count($mediStocks); $i++) {
                         echo "<td>".$stockMedicine["exp_date"]."</td>";
                         echo "<td>".$stockMedicine["enter_date"]."</td>";
                         echo "<td class='pe-3'>";
-                        echo "<a href='edit_medistock_his?id=".$stockMedicine["id"]."' class='btn btn-dark mx-2'><i class='fas fa-edit'></i></a>";
-                        echo "<a href='' class='btn btn-dark mx-2'><i class='fa fa-trash'></i></a>";
+                        echo "<a href='edit_medistock_his?id=".$stockMedicine["id"]."' class='btn btn-warning mx-2'><i class='fas fa-edit'></i></a>";
+                        // echo "<a href='' class='btn btn-dark mx-2'><i class='fa fa-trash'></i></a>";
                         echo "</td>";
                         echo"</tr>";
                     }
             ?>
-        </tbody>
-    </table>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 
-<?php
+    <?php
 
 require_once __DIR__."/./layouts/footer.php";
 
