4fc3f26410ef5717c013c53f5736bae99df5d597
Pyae Phyo Zan <pyae418283@gmail.com>
2023-03-17 16:18:10 +0630

    fix some bug for inputs

--- add_patient.php
+++ add_patient.php
@@ -2,11 +2,11 @@
 
 include_once "./layouts/header.php";
 
-if(!$auth->isAuth()){
+if (!$auth->isAuth()) {
     header('location:login_form');
 }
 
-if($auth->hasRole() == 'doctor'){
+if ($auth->hasRole() == 'doctor') {
     header('location:_403');
 }
 
@@ -14,7 +14,7 @@ require_once "./controllers/PatientController.php";
 require_once "./core/Request.php";
 require_once "./core/Validator.php";
 
-// get patient code 
+// get patient code
 $patientController = new PatientController();
 
 // add blooded type array
@@ -80,53 +80,53 @@ if (isset($_POST["add"])) {
                             <label for="" class="form-label">Name</label>
                             <input type="text" name="name" id="" class="<?php echo (isset($error_msg["name"])) ? 'form-control  border border-danger' : 'form-control'; ?>" value="<?php echo (!empty($data["name"])) ? $data["name"] : ''; ?>">
                             <?php
-                            if (isset($error_msg["name"])) {
-                                echo "<small class='text-danger'>" . $error_msg["name"] . "</small>";
-                            }
-                            ?>
+if (isset($error_msg["name"])) {
+    echo "<small class='text-danger'>" . $error_msg["name"] . "</small>";
+}
+?>
                         </div>
 
                         <div class="form-group mb-3">
                             <label for="" class="form-label">Phone</label>
                             <input type="text" name="phone" id="" class="<?php echo (isset($error_msg["phone"])) ? 'form-control  border border-danger' : 'form-control'; ?>" value="<?php echo (!empty($data["phone"])) ? $data["phone"] : ''; ?>">
                             <?php
-                            if (isset($error_msg["phone"])) {
-                                echo "<small class='text-danger'>" . $error_msg["phone"] . "</small>";
-                            }
-                            ?>
+if (isset($error_msg["phone"])) {
+    echo "<small class='text-danger'>" . $error_msg["phone"] . "</small>";
+}
+?>
                         </div>
 
                         <div class="form-group mb-3">
                             <label for="" class="form-label">Age</label>
-                            <input type="text" name="age" id="" class="form-control <?php echo (isset($error_msg["age"])) ? 'border border-danger' : ''; ?>" value="<?php (!empty($data["age"])) ? $data["age"] : ''; ?>">
+                            <input type="text" name="age" id="" class="form-control <?php echo (isset($error_msg["age"])) ? 'border border-danger' : ''; ?>" value="<?php (!empty($data["age"])) ? $data["age"] : '';?>">
                             <?php
-                            if (isset($error_msg["age"])) {
-                                echo "<small class='text-danger'>" . $error_msg["age"] . "</small>";
-                            }
-                            ?>
+if (isset($error_msg["age"])) {
+    echo "<small class='text-danger'>" . $error_msg["age"] . "</small>";
+}
+?>
                         </div>
                     </div>
 
                     <div class="col-6">
                         <div class="form-group mb-3">
                             <label for="" class="form-label">Weight(kg)</label>
-                            <input type="text" name="weight" id="" class="form-control <?php echo (isset($error_msg["weight"])) ? 'border border-danger' : ''; ?>" value="<?php (!empty($data["age"])) ? $data["age"] : ''; ?>">
+                            <input type="text" name="weight" id="" class="form-control <?php echo (isset($error_msg["weight"])) ? 'border border-danger' : ''; ?>" value="<?php (!empty($data["age"])) ? $data["age"] : '';?>">
                             <?php
-                            if (isset($error_msg["weight"])) {
-                                echo "<small class='text-danger'>" . $error_msg["weight"] . "</small>";
-                            }
-                            ?>
+if (isset($error_msg["weight"])) {
+    echo "<small class='text-danger'>" . $error_msg["weight"] . "</small>";
+}
+?>
                         </div>
 
                         <div class="form-group mb-3">
                             <label for="" class="form-label">Height(ft,in)</label>
-                            <input type="text" name="height" id="" class="form-control <?php echo (isset($error_msg["height"])) ? 'border border-danger' : ''; ?>" value="<?php (!empty($data["height"])) ? $data["height"] : ''; ?>">
+                            <input type="text" name="height" id="" class="form-control <?php echo (isset($error_msg["height"])) ? 'border border-danger' : ''; ?>" value="<?php (!empty($data["height"])) ? $data["height"] : '';?>">
                             <?php
-                            if (isset($error_msg["height"])) {
-                                echo "<small class='text-danger'>" . $error_msg["height"] . "</small>";
-                            }
+if (isset($error_msg["height"])) {
+    echo "<small class='text-danger'>" . $error_msg["height"] . "</small>";
+}
 
-                            ?>
+?>
                         </div>
 
                         <div class="form-group mb-3">
@@ -137,10 +137,10 @@ if (isset($_POST["add"])) {
                                 <option value="2" <?php echo (isset($data["gender"]) && $data["gender"] == '2') ? 'selected' : ''; ?>>Male</option>
                             </select>
                             <?php
-                            if (isset($error_msg["gender"])) {
-                                echo "<small class='text-danger'>" . $error_msg["gender"] . "</small>";
-                            }
-                            ?>
+if (isset($error_msg["gender"])) {
+    echo "<small class='text-danger'>" . $error_msg["gender"] . "</small>";
+}
+?>
                         </div>
 
                         <div class="form-group mb-3">
@@ -148,18 +148,18 @@ if (isset($_POST["add"])) {
                             <select name="blood_type" class="form-control <?php echo (isset($error_msg["blood_type"])) ? 'border border-danger' : ''; ?>">
                                 <option value="0" hidden selected>Choose Blood Type</option>
                                 <?php
-                                foreach ($bloodTypes as $bt) {
-                                ?>
+foreach ($bloodTypes as $bt) {
+    ?>
                                     <option value="<?php echo $bt; ?>" <?php echo (isset($data["blood_type"]) && $data["blood_type"] == $bt) ? 'selected' : ''; ?>><?php echo $bt; ?></option>
                                 <?php
-                                }
-                                ?>
+}
+?>
                             </select>
                             <?php
-                            if (isset($error_msg["blood_type"])) {
-                                echo "<small class='text-danger'>" . $error_msg["blood_type"] . "</small>";
-                            }
-                            ?>
+if (isset($error_msg["blood_type"])) {
+    echo "<small class='text-danger'>" . $error_msg["blood_type"] . "</small>";
+}
+?>
                         </div>
                     </div>
                 </div>
@@ -169,6 +169,39 @@ if (isset($_POST["add"])) {
     </div>
 </div>
 
+<script>
+    $('input[name="age"]').keyup(function(e){
+        
+        if(e.keyCode != 8){
+            if($(this).val() != ""){
+                let age = Number($(this).val());
+                if(!isANumber(age)){
+                    alert('age must be a number');
+                }
+                if(age > 90 || age < 0){
+                    alert('Enter possible age');
+                }
+            }
+        }
+    });
+    $('input[name="weight"]').keyup(function(e){
+        
+        if(e.keyCode != 8){
+            if($(this).val() != ""){
+                let weight = Number($(this).val());
+                if(!isANumber(weight)){
+                    alert('Weight must be a number');
+                }
+                if(weight > 400 || weight < 0){
+                    alert('Enter possible weight');
+                }
+            }
+        }
+    });
+    function isANumber(num){
+        return Number(num)? true :false;
+    }
+</script>
 <?php
 include_once "./layouts/footer.php";
 ?>
